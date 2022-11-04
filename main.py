7fd4779870267c6ff024074cd8959bd43de195b3
Cheung <cheung@tdfcu.com>
2022-11-04 16:00:28 -0400

    implemented url requests, beautiful soup web scrape, and made a list of movie titles from data collected from beautiful soup

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
+# Retrieves URL and page source
+response = requests.get(url=URL)
+page_source = response.text
 
+# Scrapes page source for movie title
+soup = BeautifulSoup(page_source, 'html.parser')
 
+movie_title = soup.find_all(name='h3', class_='title')
+
+# Makes a list of movie titles descending from 1 - 100
+movies_list = []
+for movie in movie_title:
+    movies = movie.getText()
+    movies_list.append(movies)
+reordered_movies_list = movies_list[::-1]
